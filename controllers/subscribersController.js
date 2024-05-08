abfdb2f19b9a2e2fb48cd89a2356ea2d419cdf3d
Terakawa91 <68772480+Terakawa91@users.noreply.github.com>
2024-05-08 06:23:30 +0000

    week10_capstion_fin

--- controllers/subscribersController.js
+++ controllers/subscribersController.js
@@ -1,6 +1,7 @@
 // controllers/subscribersController.js
 "use strict";
 
+const { REQUESTED_RANGE_NOT_SATISFIABLE } = require("http-status-codes");
 /**
  * @TODO:
  * Listing 16.4 (p. 230-231)
@@ -12,23 +13,64 @@
  */
 
 // 구독자 모델 요청
-const Subscriber = "";
+const Subscriber = require("../models/Subscriber");
 
 // 구독자 가져오기
 exports.getAllSubscribers = (req, res, next) => {
-  // @TODO...
+  Subscriber
+    .find({})
+    .exec()
+    .then(subscribers => {
+      console.log("Found subscribers! ", subscribers);
+      res.render(
+        "subscribers",
+        { subscribers: subscribers }
+      );
+    })
+    .catch(error => {
+      console.log(`Error: ${error.message}`);
+      return next(error);
+    });
 };
 
 // 구독 페이지 레더링
 exports.getSubscriptionPage = (req, res) => {
-  // @TODO...
+  res.render("contact");
 };
 
 // 구독자 저장
 exports.saveSubscriber = (req, res) => {
-  // @TODO...
+  let newSubscriber = new Subscriber({
+    name: req.body.name,
+    email: req.body.email,
+    phoneNumber: req.body.phoneNumber,
+    newsletter: req.body.newsletter,
+    profileImg: req.body.profileImg
+  });
+
+  newSubscriber
+    .save()
+    .then(result => {
+      res.render("thanks");
+    })
+    .catch(error => {
+      res.send(error);
+    });
 };
 
+
 exports.deleteAllSubscribers = (req, res) => {
-  // @TODO...
+  Subscriber
+    .deleteMany({})
+    .exec()
+    .then(result => {
+      res.render(
+        "subscribers",
+        { subscribers: [] }
+      )
+    })
+    .catch(error => {
+      console.log(`Error: ${error.message}`);
+      return next(error);
+    })
 };
